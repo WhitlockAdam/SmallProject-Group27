e0b3bada2efbbf63fad86d21ba8138b193c7f220
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-09-24 16:30:47 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+# Define the GitHub repository URL
+REPO_URL=https://github.com/WhitlockAdam/SmallProject-Group27.git
 
-# Go to your project directory
-cd ..
-cd var
-cd www
-cd 4331cop.com
+# Navigate to your project directory
+cd $PROJECT_DIR
 
 # Pull the latest code from GitHub
 git pull origin main
 
+# If you're using Composer for PHP dependencies, uncomment the following line
+# composer install
+
 # Restart your web server (if necessary)
+# For Apache:
 systemctl restart apache2
