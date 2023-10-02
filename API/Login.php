6d57f9f9cc3027f095c853674379a531ee06bee6
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-10-02 09:41:22 -0400

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -11,7 +11,7 @@ $firstName = "";
 $lastName = "";
 returnWithError("got here");
 // Create a new SQL database connection
-$conn = createDBConnection("localhost", "test@email.com", "testPassword", "contactmanager");
+$conn = createDBConnection("localhost", "root", "COP4331", "contactmanager");
 
 
 // Check if the connection was successful, else return an error
