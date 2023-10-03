2fda1bec5e7d956f676e2e248475d7ae6281267e
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-10-02 20:54:35 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -138,13 +138,11 @@ function showAddContactForm() {
 }
 
 function addContact() {
-    var form = document.getElementById("addContactForm");
-
-    let firstName = form.querySelector("#newFirstName").value;
-    let lastName = form.querySelector("#newLastName").value;
-    let email = form.querySelector("#newEmail").value;
-    let phoneNumber = form.querySelector("#newPhoneNumber").value;
-    let address = form.querySelector("#newAddress").value;
+    let firstName = document.getElementById("newFirstName").value;
+    let lastName = document.getElementById("newLastName").value;
+    let email = document.getElementById("newEmail").value;
+    let phoneNumber = document.getElementById("newPhoneNumber").value;
+    let address = document.getElementById("newAddress").value;
 
     let jsonPayload = {
         firstName: firstName,
@@ -165,6 +163,7 @@ function addContact() {
                 alert(response.success);
                 // Code to handle successful addition of contact
                 // You may want to refresh the contact list or perform other actions here
+                refreshContactList(); // Add this line to refresh the contact list
             } else {
                 alert(response.error);
             }
@@ -172,9 +171,67 @@ function addContact() {
     };
 
     xhr.send(JSON.stringify(jsonPayload));
+}
 
-    // Hide the form after submission
-    form.style.display = "none";
+function refreshContactList() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", urlBase + '/contacts.php?action=getAllContacts', true);
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let contacts = JSON.parse(xhr.responseText);
+            displayContacts(contacts);
+        }
+    };
+
+    xhr.send();
+}
+
+function displayContacts(contacts) {
+    let tableBody = document.getElementById("contactTableBody");
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < contacts.length; i++) {
+        let contact = contacts[i];
+        let row = document.createElement("tr");
+
+        let firstNameCell = document.createElement("td");
+        firstNameCell.innerText = contact.firstName;
+        row.appendChild(firstNameCell);
+
+        let lastNameCell = document.createElement("td");
+        lastNameCell.innerText = contact.lastName;
+        row.appendChild(lastNameCell);
+
+        let emailCell = document.createElement("td");
+        emailCell.innerText = contact.email;
+        row.appendChild(emailCell);
+
+        let phoneNumberCell = document.createElement("td");
+        phoneNumberCell.innerText = contact.phoneNumber;
+        row.appendChild(phoneNumberCell);
+
+        let addressCell = document.createElement("td");
+        addressCell.innerText = contact.address;
+        row.appendChild(addressCell);
+
+        let actionsCell = document.createElement("td");
+        let editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        editButton.onclick = function() {
+            // Add code to handle edit button click here
+        };
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.onclick = function() {
+            // Add code to handle delete button click here
+        };
+        actionsCell.appendChild(editButton);
+        actionsCell.appendChild(deleteButton);
+        row.appendChild(actionsCell);
+
+        tableBody.appendChild(row);
+    }
 }
 
 function searchContacts() {
