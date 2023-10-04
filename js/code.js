ea6e8bbd4fb78a015b6e5963e9afff40d489283c
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-10-04 16:26:58 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -103,6 +103,8 @@ function saveCookie()
 	let minutes = 20;
 	let date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
+	console.log(userIf);
+	console.log(firstName);
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
